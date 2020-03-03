cc552a58be03186db95e543998ab4bcb712680eb
William <wpdcastro@gmail.com>
2020-03-02 21:04:01 -0300

    Perceptron para reconhecer A e T - mk1

--- perceptron_alfabeto.py
+++ perceptron_alfabeto.py
@@ -1,27 +1,28 @@
 ''' Perceptron '''
 ''' @author : wpdcastro@gmail.com '''
+import random
 
 class Perceptron:
 
     def __init__(self, n, w=[1,-1]):
+        # n = numero de entradas
         self.n = n
+        # w = numero de pesos
         self.w = list()
         self.w.append(1)
-        self.set_pesos(w)
         self.eta = 0.2
 
-    def set_pesos(self, w):
-        
-        for peso in w :
-            self.w.append(peso)
+        for i in range(n) :
+            valor = random.randint(0,1) 
+            self.w.append(valor)    
 
     def soma(self, entrada):
-        s = self.w[0]
+        
+        soma = self.w[0]
 
-        soma = 0;
-        for idx, val in enumerate(entrada) :
-            if len(entrada) < idx+1 :
-                soma += entrada[idx] * entrada[idx]
+        for idx in range(1, len(self.w)) :
+
+            soma += entrada[idx-1] * self.w[idx]
 
         return soma
 
@@ -34,27 +35,60 @@ class Perceptron:
 
     def treinar(self, tabela, res_esperado):
 
-        for idx, linha in tabela :
-            
+        for idx in range (0, len(tabela)) :
+
             e = res_esperado[idx] - self.y(tabela[idx])
 
-            if res_esperado[idx] != e :
+            if e != 0 :
+                
+                self.w[0] = self.w[0] + self.eta * e
 
-                for i, esperado in enumerate(self.w):
-                    self.w[i] = self.w[i] + self.eta * e * res_esperado[idx]
+                for i in range(1,len(self.w)):
+                    #w[i] = w[i] + eta * e * tabela[idx][i]
+                    #if len(self.w) < len(tabela[idx]) :
+                    self.w[i] = self.w[i] + self.eta * e * tabela[idx][i-1]
+                    #print(len(self.w), len(res_esperado), len(tabela[idx]))
 
         return False
 
 
 if __name__ == "__main__":
 
-    percy = Perceptron(2)
-    pad = [[0, 0], [0, 1], [1,0], [1, 1]]
-    res_esperado = [0, 0, 0, 1]
+    percy0 = Perceptron(25)
+    percy1 = Perceptron(25)
+    percy2 = Perceptron(25)
+    percy3 = Perceptron(25)
+    percy4 = Perceptron(25)
 
-    percy.treinar(pad, res_esperado)
+    pad = [[1, 1, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1],
+           [1, 1, 1, 1, 1,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0]
+            ]
 
-    print("Y: " + str(percy.y([0, 0])))
-    print("Y: " + str(percy.y([0, 1])))
-    print("Y: " + str(percy.y([1, 0])))
-    print("Y: " + str(percy.y([1, 1])))
+    res_esperado = [0,1]
+    #res_percy0 = 
+            
+
+    percy0.treinar(pad, res_esperado)
+
+    print("Y: " + str(percy0.y([1, 1, 1, 1, 1,
+                                1, 0, 0, 0, 1,
+                                1, 1, 1, 1, 1,
+                                1, 0, 0, 0, 1,
+                                1, 0, 0, 0, 1])))
+    print("Y: " + str(percy0.y([1, 1, 1, 1, 1,
+                                0, 0, 1, 0, 0,
+                                0, 0, 1, 0, 0,
+                                0, 0, 1, 0, 0,
+                                0, 0, 1, 0, 0])))
+
+#    print("Y: " + str(percy.y([0, 1])))
+#    print("Y: " + str(percy.y([1, 0])))
+#    print("Y: " + str(percy.y([1, 1])))
